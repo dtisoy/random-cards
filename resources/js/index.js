74c75aa1d2a2cc503c9b24da399ebfea9a248500
Daniel Tisoy <danieltisoy2001@gmail.com>
2023-10-04 16:50:26 -0500

    add: get data from github
    set img, name, github url in the card

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,30 @@
-fetch('./content/profiles.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+const APIURL = 'https://api.github.com/users/'
+const PROFILESDATA = "resources/data/profiles.json"
+
+const cardImg = document.getElementById("user-img");
+const cardName = document.getElementById("user-name");
+const cardGithubAnchor = document.getElementById("github-anchor")
+
+
+
+let getLocalUsers = async () => {
+    let usersData = await fetch(PROFILESDATA);
+    let usersJson = await usersData.json();
+    return usersJson;
+}
+
+let getRandomProfile = async () => {
+    let profiles = await getLocalUsers();
+    let randomProfile = profiles[Math.floor(Math.random() * profiles.length)];
+    return randomProfile;
+}
+let getGithubInfo = async () => {
+    let user = await getRandomProfile();
+    fetch(APIURL + user.username).then(response => response.json()).then(json => {
+        cardImg.src = json["avatar_url"];
+        cardName.textContent = user["name"];
+        cardGithubAnchor.href = json["html_url"];
+    })
+}
+
+getGithubInfo()
